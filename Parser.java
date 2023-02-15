25e1557df622f5fbe249904ddb8809b0c5171b65
truescotian <gregpmillr@gmail.com>
2023-02-14 21:00:25 -0400

    More reading on recursive descent parsers. On page 88 now learning about panic mode error recovery

--- Parser.java
+++ Parser.java
@@ -4,6 +4,18 @@ import java.util.List;
 
 import static jlox.TokenType.*;
 
+/**
+ *  A parser has two jobs:
+ *  1. Given a valid sequence of tokens, produce a corresponding syntax tree.
+ *  2. Given an invalid sequence of tokens, detect any errors and tell 
+ *  the user about their mistakes.
+ *
+ *  #2 is very important. In modern IDEs, the parser is always reparsing the code
+ *  even while you're editing it in order to syntax highlight and support things
+ *  like auto-complete. When the user doesn't know syntax is wrong, it's up to
+ *  the parser to help guide them back onto the right path.
+ */
+
 class Parser {
 	private final List<Token> tokens;
 	private int current = 0;
@@ -16,7 +28,7 @@ class Parser {
 		return equality();
 	}
 
-	// comparison ( ( "!=" | "==" ) comparison )*
+	// rule: comparison ( ( "!=" | "==" ) comparison )*
 	// this created a left-associative nested tree of binary
 	// operator nodes. This is because given a == b == c == d == e
 	// we zip through the sequence of equality expressions til the end
@@ -48,6 +60,72 @@ class Parser {
 
 		return expr;
 	}
+
+	// identical to equality() except for the token types for the operators
+	// and the methods for the operands -- now term() instead of comparison()
+	private Expr comparison() {
+		Expr expr = term();
+
+		while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+			Token operator = previous();
+			Expr right = term();
+			expr = new Expr.Binary(expr, operator, right);
+		}
+
+		return expr;
+	}
+
+	// rule: factor ( ( "-" | "+" ) factor )*
+	private Expr term() {
+		Expr expr = factor();
+
+		while (match(MINUS, PLUS)) {
+			Token operator = previous();
+			Expr right = factor();
+			expr = new Expr.Binary(expr, operator, right);
+		}
+
+		return expr;
+	}
+
+	// unary ( ( "/" | "*" ) factor )*
+	private Expr factor() {
+		Expr expr = unary();
+
+		while (match(SLASH, STAR)) {
+			Token operator = previous();
+			Expr right = unary();
+			expr = new Expr.Binary(expr, operator, right);
+		}
+	}
+
+	// rule: ( "!" | "-" ) unary
+	private Expr unary() {
+		if (match(BANG, MINUS)) {
+			Token operator = previous();
+			Expr right = unary();
+			return new Expr.Unary(operator, right);
+		}
+
+		return primary();
+	}
+
+	// rule: NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")"
+	private Expr primary() {
+		if (match(FALSE)) return new Expr.Literal(false);
+		if (match(TRUE)) return new Expr.Literal(true);
+		if (match(NIL)) return new Expr.Literal(null);
+
+		if (match(NUMBER, STRING)) {
+			return new Expr.Literal(previous().literal);
+		}	
+
+		if (match(LEFT_PAREN)) {
+			Expr expr = expression();
+			consume(RIGHT_PAREN, "Expect ')' after expression.");
+			return new Expr.Grouping(expr);
+		}
+	}	
 
 	// Checks to see if the current token has any of the given types.
 	// If so, consume the token and return true, otherwise return
@@ -87,6 +165,6 @@ class Parser {
 
 	// returns the most recently consumed token
 	private Token previous() {
-		return token.get(current -1);
+		return tokens.get(current -1);
 	}
 }
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Within parent dir, type "java subdir/filename"
 
 # Codegen
 Generate expressions on types using `./tool/GenerateAst <output_dir>`
+
+# Faq
+1. Wtf is recursive descent? youtube.com/watch?v=eVwl0hkI21Y
