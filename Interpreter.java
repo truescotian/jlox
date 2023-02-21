d3ab86ed7781b6ab4f0c64ffcc4a475c415cff22
truescotian <gregpmillr@gmail.com>
2023-02-20 22:04:05 -0400

    implemented more binary operator types

--- Interpreter.java
+++ Interpreter.java
@@ -40,4 +40,55 @@ class Interpreter implements Expr.Visitor<Object> {
 	private Object evaluate(Expr expr) {
 		return expr.accept(this);
 	}
+
+	@Override
+	public Object visitBinaryExpr(Expr.Binary expr) {
+		Object left = evaluate(expr.left);
+		Object right = evaluate(expr.right);
+
+		switch (expr.operator.type) {
+			case BANG_EQUAL: return !isEqual(left, right);
+			case EQUAL_EQUAL: return isEqual(left, right);
+			case GREATER:
+				return (double)left > (double)right;
+			case GREATER_EQUAL:
+				return (double)left >= (double)right;
+			case LESS:
+				return (double)left < (double)right;
+			case LESS_EQUAL:
+				return (double)left <= (double)right;
+			case MINUS:
+				return (double)left - (double)right;
+			case PLUS:
+				if (left instanceof Double && right instanceof Double) {
+					return (double)left + (double)right;
+				}
+
+				if (left instanceof String && right instanceof String) {
+					return (String)left + (String)right;
+				}
+
+				break;
+			case SLASH:
+				return (double)left / (double)right;
+			case STAR:
+				return (double)left * (double)right;
+		}
+
+		// Unreachable
+		return null;
+	}
+
+	private boolean isTruthy(Object object) {
+		if (object == null) return false;
+		if (object instanceof Boolean) return (boolean)object;
+		return true;
+	}
+
+	private boolean isEqual(Object a, Object b) {
+		if (a == null && b == null) return true;
+		if (a == null) return false;
+
+		return a.equals(b);
+	}
 }
