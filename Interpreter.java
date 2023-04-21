11176fbd66d59ae20241fa91386bc8f8596d1ea5
truescotian <gregpmillr@gmail.com>
2023-04-20 21:40:09 -0300

    Add a clock class which implements LoxCallable

--- Interpreter.java
+++ Interpreter.java
@@ -1,9 +1,29 @@
 package jlox;
 
 import java.util.List;
+import java.util.ArrayList;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-	private Environment environment = new Environment();
+	final Environment globals = new Environment();
+	private Environment environment = globals;
+
+	Interpreter() {
+		// define a variable named "clock" which implementes
+		// LoxCallable.
+		globals.define("clock", new LoxCallable() {
+			@Override
+			public int arity() { return 0; }
+
+			@Override
+			public Object call(Interpreter interpreter,
+					List<Object> arguments) {
+				return (double)System.currentTimeMillis() / 1000.0;
+			}
+
+			@Override
+			public String toString() { return "<native fn>"; }
+		});
+	}
 
 	void interpret(List<Stmt> statements) {
 		try {
@@ -55,6 +75,35 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 		// Unreachable.
 		return null;
+	}
+
+	@Override
+	public Object visitCallExpr(Expr.Call expr) {
+
+		// evaluate the expression for the callee. This is typically
+		// an identifier that looks up the function by its name, but it
+		// could be anything.
+		Object callee = evaluate(expr.callee);
+
+		List<Object> arguments = new ArrayList<>();
+		for (Expr argument : expr.arguments) {
+			arguments.add(evaluate(argument));
+		}
+
+		// prevent ClassCastException by ensuring callee is a LoxCallable
+		if (!(callee instanceof LoxCallable)) {
+			throw new RuntimeError(expr.paren, 
+					"Can only call functions and classes");
+		}
+
+		LoxCallable function = (LoxCallable)callee;
+
+		if (arguments.size() != function.arity()) {
+			throw new RuntimeError(expr.paren, "Expected " +
+					function.arity() + " arguments but got " +
+					arguments.size() + ".");
+		}
+		return function.call(this, arguments);
 	}
 
 	@Override
