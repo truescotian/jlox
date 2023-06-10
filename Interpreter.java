683c8b7b4b348944e59f6f6059ebce37efa90110
truescotian <gregpmillr@gmail.com>
2023-06-10 17:33:04 -0300

    Completed Get and Set for classes. That puts me over 200 pages of the book :). On to methods now

--- Interpreter.java
+++ Interpreter.java
@@ -58,6 +58,20 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	}
 
 	@Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+
+    if (!(object instanceof LoxInstance)) { 
+      throw new RuntimeError(expr.name,
+                             "Only instances have fields.");
+    }
+
+    Object value = evaluate(expr.value);
+    ((LoxInstance)object).set(expr.name, value);
+    return value;
+  }
+
+	@Override
 	public Object visitUnaryExpr(Expr.Unary expr) {
 		// we can't evaluate the unary operator until we evaluate
 		// it's operand subexpression. This means the interpreter
@@ -108,6 +122,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		}
 		return function.call(this, arguments);
 	}
+
+	@Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name,
+        "Only instances have properties.");
+  }
 
 	@Override
 	public Object visitVariableExpr(Expr.Variable expr) {
--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -16,8 +16,10 @@ public class GenerateAst {
 			"Assign : Token name, Expr value",
 			"Binary   : Expr left, Token operator, Expr right",
 			"Call : Expr callee, Token paren, List<Expr> arguments",
+			"Get      : Expr object, Token name",
 			"Grouping : Expr expression",
 			"Logical : Expr left, Token operator, Expr right",
+			"Set : Expr object, Token name, Expr value",
 			"Literal  : Object value",
 			"Unary    : Token operator, Expr right",
 			"Variable : Token name"
