e118cb871537cb5ad745e793f3e17f3759a45678
truescotian <gregpmillr@gmail.com>
2023-06-10 13:40:02 -0300

    Environment walking and using the resolver in the interpreter

--- Interpreter.java
+++ Interpreter.java
@@ -1,11 +1,14 @@
 package jlox;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	final Environment globals = new Environment();
 	private Environment environment = globals;
+	private final Map<Expr, Integer> locals = new HashMap<>();
 
 	Interpreter() {
 		// define a variable named "clock" which implementes
@@ -108,13 +111,20 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	@Override
 	public Object visitVariableExpr(Expr.Variable expr) {
-		Object value = environment.get(expr.name);
-		if (value == null) {
-			throw new RuntimeError(expr.name,
-					"Variable must be initialized before use.");
-		}
-		return value;
+		return lookupVariable(expr.name, expr);
 	}
+
+	private Object lookupVariable(Token name, Expr expr) {
+		// look up the resolved distance in the map.
+		// remember that globals are treated specially and don't
+		// end up in the map. Hence the name locals.
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else {
+      return globals.get(name);
+    }
+  }
 
 	private void checkNumberOperand(Token operator, Object operand) {
 		if (operand instanceof Double) return;
@@ -169,6 +179,10 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	private void execute(Stmt stmt) {
 		stmt.accept(this);
 	}
+
+	void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
+  }
 
 	// executes a list of statements in the context of a given environment.
 	void executeBlock(List<Stmt> statements, Environment environment) {
@@ -254,7 +268,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	@Override
 	public Object visitAssignExpr(Expr.Assign expr) {
 		Object value = evaluate(expr.value);
-		environment.assign(expr.name, value);
+
+		Integer distance = locals.get(expr);
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
 		return value;
 	}
 
--- Resolver.java
+++ Resolver.java
@@ -28,11 +28,40 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    resolve(stmt.expression);
+    return null;
+  }
+
+  @Override
   public Void visitFunctionStmt(Stmt.Function stmt) {
     declare(stmt.name);
     define(stmt.name);
 
     resolveFunction(stmt);
+    return null;
+  }
+
+  @Override
+  public Void visitIfStmt(Stmt.If stmt) {
+    resolve(stmt.condition);
+    resolve(stmt.thenBranch);
+    if (stmt.elseBranch != null) resolve(stmt.elseBranch);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    resolve(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitReturnStmt(Stmt.Return stmt) {
+    if (stmt.value != null) {
+      resolve(stmt.value);
+    }
+
     return null;
   }
 
@@ -56,12 +85,61 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Void visitWhileStmt(Stmt.While stmt) {
+    resolve(stmt.condition);
+    resolve(stmt.body);
+    return null;
+  }
+
+  @Override
   public Void visitAssignExpr(Expr.Assign expr) {
     // resolve the expression for the assigned value in case it also
     // contains references to other variables.
     resolve(expr.value);
     // resolve the variable that's being assigned to.
     resolveLocal(expr, expr.name);
+    return null;
+  }
+
+  @Override
+  public Void visitBinaryExpr(Expr.Binary expr) {
+    resolve(expr.left);
+    resolve(expr.right);
+    return null;
+  }
+
+  @Override
+  public Void visitCallExpr(Expr.Call expr) {
+    resolve(expr.callee);
+
+    for (Expr argument : expr.arguments) {
+      resolve(argument);
+    }
+
+    return null;
+  }
+
+  @Override
+  public Void visitGroupingExpr(Expr.Grouping expr) {
+    resolve(expr.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitLiteralExpr(Expr.Literal expr) {
+    return null;
+  }
+
+  @Override
+  public Void visitLogicalExpr(Expr.Logical expr) {
+    resolve(expr.left);
+    resolve(expr.right);
+    return null;
+  }
+
+  @Override
+  public Void visitUnaryExpr(Expr.Unary expr) {
+    resolve(expr.right);
     return null;
   }
 
